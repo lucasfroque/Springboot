acd659c5f66d3894a570ee7bd423bb716e1192e7
lucasfroque <lucasfernandolf38@gmail.com>
2022-04-26 16:01:36 -0300

    add Category

---
+++ src/main/java/com/lucas/project/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.lucas.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucas.project.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
