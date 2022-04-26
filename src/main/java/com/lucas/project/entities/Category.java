70cb825a231d104ba71caa6d1fe25b18fc1e8359
lucasfroque <lucasfernandolf38@gmail.com>
2022-04-26 17:32:19 -0300

    add Products

--- src/main/java/com/lucas/project/entities/Category.java
+++ src/main/java/com/lucas/project/entities/Category.java
@@ -1,13 +1,18 @@
 package com.lucas.project.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Entity
@@ -19,6 +24,11 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 		
@@ -51,6 +61,10 @@ public class Category implements Serializable{
 		return Objects.hash(id);
 	}
 
+	public Set<Product> getProducts() {
+		return products;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -62,7 +76,4 @@ public class Category implements Serializable{
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
 }
---
+++ src/main/java/com/lucas/project/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.lucas.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucas.project.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
